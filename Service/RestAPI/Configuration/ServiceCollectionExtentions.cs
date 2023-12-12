267b2b883ca039df5c24ce46d9e0851d654d278f
samuhuse <samuele.mn@hotmail.it>
2023-12-12 15:10:58 +0100

    added Rest APIs service

---
+++ Service/RestAPI/Configuration/ServiceCollectionExtentions.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using Application;
+using AuthProvider.ACME;
+using AuthProvider.Globex;
+using AuthProvider.MassiveDynamic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RestAPI.Configuration;
+
+public static class ServiceCollectionExtensions  
+{  
+    public static IServiceCollection AddAuthProviders(this IServiceCollection serviceCollection)
+    {
+        ForceInfrastructureAssembliesLoad();
+        
+        var authProviderTypes =   
+            AppDomain.CurrentDomain.GetAssemblies()  
+                .SelectMany(assembly => assembly.GetTypes())  
+                .Where(type => typeof(IAuthProvider).IsAssignableFrom(type) && type is { IsInterface: false, IsAbstract: false });  
+        
+        foreach (var authProviderType in authProviderTypes) serviceCollection.AddScoped(authProviderType);
+        
+        return serviceCollection;  
+    }
+
+    private static void ForceInfrastructureAssembliesLoad()
+    {
+        foreach (var name in Directory.GetFiles(
+                     AppDomain.CurrentDomain.BaseDirectory, "*.dll").Where(r => 
+                     !AppDomain.CurrentDomain.GetAssemblies().ToList().Select(a => 
+                         a.Location).ToArray().Contains(r, StringComparer.InvariantCultureIgnoreCase)).ToList())
+        {
+            Assembly.Load(Path.GetFileNameWithoutExtension(new FileInfo(name).Name));
+        }
+    }
+    
+}
--- Service/RestAPI/DefaultAuthService.cs
+++ Service/RestAPI/DefaultAuthService.cs
@@ -1,3 +1,4 @@
+using System;
 using Application;
 using AuthProvider.ACME;
 using AuthProvider.Globex;
@@ -5,18 +6,16 @@ using AuthProvider.MassiveDynamic;
 
 namespace RestAPI;
 
-public class DefaultAuthService : AuthService
-{
-    //  Invoked in this sequence
-    static readonly Type[] AuthProviders = new[]
-    {
-        typeof(ACMEAuthProvider),
-        typeof(MassiveDynamicAuthProvider),
-        typeof(GlobexAuthProvider)
-        // ...
-    };
+public class DefaultAuthService : AuthService  
+{  
+    //  Invoked in this sequence  
+    static readonly Type[] AuthProviders = new[]  
+    {  
+        typeof(ACMEAuthProvider),  
+        typeof(MassiveDynamicAuthProvider),  
+        typeof(GlobexAuthProvider)  
+        // ...  
+    };  
     
-    public DefaultAuthService(IServiceProvider serviceProvider) : base(serviceProvider, AuthProviders)
-    {
-    }
+    public DefaultAuthService(IServiceProvider serviceProvider) : base(serviceProvider, AuthProviders) { }
 }
